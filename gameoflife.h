720fb1b090a0b555fbbb284ac4aefdea77575677
shyung18 <47002618+shyung18@users.noreply.github.com>
2019-02-06 20:11:21 -0500

    Header

---
+++ gameoflife.h
@@ -0,0 +1,20 @@
+static const int activeRows = 18;
+static const int activeCols = 50;
+static const int totalRows = activeRows + 2;
+static const int totalCols = activeCols + 2;
+
+enum class Organism { NONE, LIVING };
+
+class Board
+{
+  public:
+    Board();
+    void printBoard();
+    void setInitialBoard(int xValues[], int yValues[], int numOrg);
+    int getNumNeighbors(int x, int y);
+    void generation();
+    Organism getState(int x, int y);
+
+  private:
+    Organism _board[totalRows][totalCols];
+};
